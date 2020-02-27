2f916d0df4cd0ce9f6910f36aa8d9ca0a5b79a2f
dominicdaviescodes <codewithdom@gmail.com>
2020-02-27 22:32:29 +0000

    display validation error messages for create and updating events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -19,8 +19,11 @@ class EventsController < ApplicationController
 
   def update
     @event = Event.find(params[:id])
-    @event.update(event_params)
-    redirect_to @event
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render :edit
+    end
   end
 
   def new
@@ -29,8 +32,11 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(event_params)
-    @event.save
-    redirect_to @event
+    if @event.save
+      redirect_to @event
+    else 
+      render :new
+    end
   end
 
   def destroy
