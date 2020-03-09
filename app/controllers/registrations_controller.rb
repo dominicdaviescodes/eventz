610388487ddfbef7a32bbd4f855fff89d73c089d
dominicdaviescodes <codewithdom@gmail.com>
2020-03-09 22:02:37 +0000

    add register button to register for event

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,27 @@ class RegistrationsController < ApplicationController
     @event = Event.find(params[:event_id])
     @registrations = @event.registrations
   end
+
+  def new
+    @event = Event.find(params[:event_id])
+    @registration = @event.registrations.new
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @registration = @event.registrations.new(registration_params)
+
+    if @registration.save
+      redirect_to event_registrations_url(@event),
+        notice: "Thanks for registering!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def registration_params 
+    params.require(:registration).permit(:name, :email, :how_heard)
+  end
 end
