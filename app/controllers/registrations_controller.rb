3fa88d397a03411951b302ca13526b22d2280b2e
dominicdaviescodes <codewithdom@gmail.com>
2020-03-18 22:48:24 +0000

    refactor code in reg controller using before action

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,17 +1,16 @@
 class RegistrationsController < ApplicationController
 
+  before_action :set_event
+
   def index
-    @event = Event.find(params[:event_id])
     @registrations = @event.registrations
   end
 
   def new
-    @event = Event.find(params[:event_id])
     @registration = @event.registrations.new
   end
 
   def create
-    @event = Event.find(params[:event_id])
     @registration = @event.registrations.new(registration_params)
 
     if @registration.save
@@ -26,5 +25,9 @@ class RegistrationsController < ApplicationController
 
   def registration_params 
     params.require(:registration).permit(:name, :email, :how_heard)
+  end
+
+  def set_event
+    @event = Event.find(params[:event_id])
   end
 end
